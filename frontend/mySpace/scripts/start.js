c365e4903a6bc3056fa33cf33fe79b0e03f129ba
ArthasDragon <1641627793@qq.com>
2018-12-20 10:43:20 +0800

    prepare

--- frontend/mySpace/scripts/start.js
+++ frontend/mySpace/scripts/start.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+
+const webpack = require("webpack");
+const devWebpackConf = require("../config/webpack.dev.conf");
+
+const devMiddleware = require("webpack-dev-middleware");
+const hotMiddleware = require("webpack-hot-middleware");
+
+const compiler = webpack(devWebpackConf);
+
+app.use(devMiddleware(compiler));
+app.use(hotMiddleware(compiler));
