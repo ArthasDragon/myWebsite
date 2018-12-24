8c4fad035511642807bf2099a8137cebe0c1a13b
ArthasDragon <1641627793@qq.com>
2018-12-24 10:20:54 +0800

    prepare

--- frontend/mySpace/utils/common/env.js
+++ frontend/mySpace/utils/common/env.js
@@ -1,7 +1,9 @@
+//get current NODE_ENV
 exports.getenv = function() {
   return process.env.NODE_ENV || "dev";
 };
 
+//judge the environment
 exports.isBuild = function() {
   return global.mode === "build";
 };
