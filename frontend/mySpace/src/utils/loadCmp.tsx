d2306537d72fab15fe0bfaec994bd433d40e4fa0
ArthasDragon <1641627793@qq.com>
2018-12-28 13:48:50 +0800

    prepare

--- frontend/mySpace/src/utils/loadCmp.tsx
+++ frontend/mySpace/src/utils/loadCmp.tsx
@@ -4,5 +4,5 @@ import * as Loadable from "react-loadable";
 export const asyncLoad = (path: "String") =>
   Loadable({
     loader: () => import(path),
-    loading: () => <div>加载中...</div>
+    loading: () => <div>加载中...</div>,
   });
