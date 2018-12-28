babf2df6b879f152bb7902ce999a77efdb5ec8b6
ArthasDragon <1641627793@qq.com>
2018-12-28 10:49:22 +0800

    prepare

---
+++ frontend/mySpace/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Home extends React.Component {
+  private render() {
+    return (
+      <div className="App">
+        <header>454545</header>
+      </div>
+    );
+  }
+}
+
+export default Home;
