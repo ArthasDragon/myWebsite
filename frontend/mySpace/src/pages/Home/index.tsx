5d7a616e8476d6f45ce73a05f0ddddb79d28135d
unknown <wb-ywl538541@hz.ali.com>
2019-03-28 17:51:42 +0800

    js to ts

--- frontend/mySpace/src/pages/Home/index.tsx
+++ frontend/mySpace/src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import * as React from 'react';
 
 class Home extends React.Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header>45434545</header>
