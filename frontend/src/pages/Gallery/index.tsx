42bb2b4c41bda178744e2f683d27a9014cce6387
ArthasDragon <1641627793@qq.com>
2019-11-28 10:26:00 +0800

    gallery

--- frontend/src/pages/Gallery/index.tsx
+++ frontend/src/pages/Gallery/index.tsx
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import './gallery.less';
 
 function Bus() {
-  const [count, setCount] = useState<number>(0);
+  const [count] = useState<number>(0);
 
   useEffect(() => {
     document.title = `You Clicked ${count} times`;
   });
 
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+    <div className="gallery">
+      {/* 头部 */}
+      <div className="header" />
+
+      {/* 封面list */}
+      <div className="cover_list">
+        {/* 封面 */}
+        <div className="cover">
+          {/* 封面内蒙版  按钮 */}
+          <div className="content">
+            {/* 查看按钮 */}
+            <div className="link">view</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
